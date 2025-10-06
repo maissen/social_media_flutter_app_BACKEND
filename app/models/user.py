e47369ebea177e6b8690ba2414459553f7ca3d0a
Maissen <maissen.developer500@gmail.com>
2025-10-06 19:45:02 +0100

    Create user model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Date
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    profile_picture = Column(String, nullable=True)
