2de8fcf2774fe9874d058b5507a2b74c65f2c246
Maissen <maissen.developer500@gmail.com>
2025-10-06 20:12:38 +0100

    Update

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,22 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from app.core.config import settings
 
-# Use Argon2 instead of bcrypt
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: int = 3600):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(seconds=expires_delta)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
